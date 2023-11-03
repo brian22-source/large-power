c21690b62df20e23ea7c4e7ded5c10d8c3ae5c34
19carty <143806608+19carty@users.noreply.github.com>
2023-11-03 22:32:30 +0300

    divisible

---
+++ divisible by 10.py
@@ -0,0 +1,9 @@
+def divisible_by_ten(num):
+    return num % 10 == 0
+print(divisible_by_ten(20))  # True, because 20 is divisible by 10
+print(divisible_by_ten(15))  # False, because 15 is not divisible by 10
+print(divisible_by_ten(100))  # True, because 100 is divisible by 10
+
+
+
+
